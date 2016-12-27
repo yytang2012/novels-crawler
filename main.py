8f47a7d0f37f345f9948f8ad3d578cc92737a6bd
yytang <yytang@email.wm.edu>
2016-12-27 16:24:56 -0500

    First commit. This commit is only a test of scrapy tools runing within a script

---
+++ main.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(get_project_settings())
+
+
+start_urls = [
+    'http://quotes.toscrape.com/',
+]
+process.crawl('toscrape-css', start_urls=start_urls)
+process.start()  # the script will block here until the crawling is finished
