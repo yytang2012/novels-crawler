b8c70c7a9bece1c43e1a0c9cd616f36ab5928805
yytang <kissingers800@gmail.com>
2018-01-20 22:16:17 -0500

    www.lushuwu.com

---
+++ novelsCrawler/spiders/lushuwu.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Created on April 15 2017
+
+@author: yytang
+"""
+
+from scrapy import Selector
+
+from libs.misc import get_spider_name_from_domain
+from libs.polish import polish_title, polish_subtitle, polish_content
+from novelsCrawler.spiders.novelSpider import NovelSpider
+
+
+class LushuwuSpider(NovelSpider):
+    """
+    classdocs
+
+    example: http://www.lushuwu.com/7/7196/
+    """
+
+    allowed_domains = ['www.lushuwu.com']
+    name = get_spider_name_from_domain(allowed_domains[0])
+
+    def parse_title(self, response):
+        sel = Selector(response)
+        title = sel.xpath('//h1/text()').extract()[0]
+        title = polish_title(title, self.name)
+        return title
+
+    def parse_episoders(self, response):
+        sel = Selector(response)
+        episoders = []
+        subtitle_selectors = sel.xpath('//ul[@class="chapters"]/li/a')
+        for page_id, subtitle_selector in enumerate(subtitle_selectors):
+            subtitle_url = subtitle_selector.xpath('@href').extract()[0]
+            subtitle_url = response.urljoin(subtitle_url.strip())
+            subtitle_name = subtitle_selector.xpath('text()').extract()[0]
+            subtitle_name = polish_subtitle(subtitle_name)
+            episoders.append((page_id, subtitle_name, subtitle_url))
+        return episoders
+
+    def parse_content(self, response):
+        sel = Selector(response)
+        content = sel.xpath('//div[@id="content"]/text()').extract()
+        content = polish_content(content)
+        return content
